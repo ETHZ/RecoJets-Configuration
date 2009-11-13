82db7a91cdcc9e50150de1bcce636ef86e1f2889
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-11-13 14:29:30 +0000

    Backing out JPT changes but adding bug fixes

--- python/GenJetParticles_cff.py
+++ python/GenJetParticles_cff.py
@@ -1,16 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
 #
-# $Id: GenJetParticles_cff.py,v 1.2 2008/04/21 03:27:15 rpw Exp $
+# $Id: GenJetParticles_cff.py,v 1.5 2009/11/11 21:02:37 srappocc Exp $
 #
 # ShR 27 Mar 07: move modules producing candidates for Jets from RecoGenJets.cff
 # 
 #
 genParticlesForJets = cms.EDFilter("InputGenJetsParticleSelector",
     src = cms.InputTag("genParticles"),
-    ignoreParticleIDs = cms.vuint32(1000022, 2000012, 2000014, 2000016, 1000039, 
-        5000039, 4000012, 9900012, 9900014, 9900016, 
-        39),
+    ignoreParticleIDs = cms.vuint32(
+         1000022,
+         1000012, 1000014, 1000016,
+         2000012, 2000014, 2000016,
+         1000039, 5100039,
+         4000012, 4000014, 4000016,
+         9900012, 9900014, 9900016,
+         39),
     partonicFinalState = cms.bool(False),
     excludeResonances = cms.bool(True),
     excludeFromResonancePids = cms.vuint32(12, 13, 14, 16),
@@ -21,3 +26,9 @@ genJetParticles = cms.Sequence(genParticlesForJets)
 
 hiGenParticlesForJets = genParticlesForJets.clone()
 hiGenParticlesForJets.src = cms.InputTag("hiGenParticles")
+
+genParticlesForJetsNoNu = genParticlesForJets.clone()
+genParticlesForJetsNoNu.ignoreParticleIDs += cms.vuint32( 12,14,16)
+
+genParticlesForJetsNoMuNoNu = genParticlesForJets.clone()
+genParticlesForJetsNoMuNoNu.ignoreParticleIDs += cms.vuint32( 12,13,14,16)
