930e88782d4d893bbb4dfff3d36031da1598d7dc
cmsbuild <>
2013-05-02 14:54:01 +0000

    This commit was manufactured by cvs2git to create tag 'RecoJets- Configuration-V02-10-06'.
    Sprout from master 2013-05-02 14:53:59 UTC Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch> 'Apparently several AlCa and DQM packages are still using KT6CaloJets'
    Cherrypick from master 2011-06-10 08:15:55 UTC Hartmut Stadie <sha1-c6dd0fe7a2ac436a11102a68e1faace3f3323990@cern.ch> 'remove ic5 JPT jets from default jet reconstruction':
        python/RecoJPTJets_cff.py

--- python/RecoJPTJets_cff.py
+++ python/RecoJPTJets_cff.py
@@ -1,16 +1,9 @@
-# $Id: RecoJPTJets_cff.py,v 1.5 2013/04/30 09:04:39 kodolova Exp $
-
-from RecoJets.JetAssociationProducers.ak5JTA_cff import *
-ak5JetTracksAssociatorAtVertexJPT = ak5JetTracksAssociatorAtVertex.clone()
-ak5JetTracksAssociatorAtVertexJPT.useAssigned = cms.bool(True)
-ak5JetTracksAssociatorAtVertexJPT.pvSrc = cms.InputTag("offlinePrimaryVertices")
-
+# $Id: RecoJPTJets_cff.py,v 1.3 2010/03/21 19:05:55 dlange Exp $
 from RecoJets.JetPlusTracks.JetPlusTrackCorrections_cff import *
-#JetPlusTrackCorrectionsAntiKt5.ptCUT = 15.
 
 #define jetPlusTrackZSPCorJet sequences
 jetPlusTrackZSPCorJetIcone5   = cms.Sequence(JetPlusTrackCorrectionsIcone5)
 jetPlusTrackZSPCorJetSiscone5 = cms.Sequence(JetPlusTrackCorrectionsSisCone5)
 jetPlusTrackZSPCorJetAntiKt5  = cms.Sequence(JetPlusTrackCorrectionsAntiKt5)
  
-recoJPTJets=cms.Sequence(ak5JetTracksAssociatorAtVertexJPT*jetPlusTrackZSPCorJetAntiKt5)
+recoJPTJets=cms.Sequence(jetPlusTrackZSPCorJetAntiKt5)
